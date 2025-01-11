23ba6c8bdd9297aca00b73fdb23950184f98786d
AvazbekHasanov <avazbekxasanov200@gmail.com>
2025-01-11 12:38:35 +0500

    added delete oportunity

--- src/main.js
+++ src/main.js
@@ -19,4 +19,4 @@ const i18n = createI18n({
 });
 // http://localhost:3000"
 // https://timetable-backend-a5bm.onrender.com
-createApp(DemoApp).use(i18n).provide("baseUrl",  "http://localhost:3000" ).use(router).mount('#app')
+createApp(DemoApp).use(i18n).provide("baseUrl",  "https://timetable-backend-a5bm.onrender.com" ).use(router).mount('#app')
